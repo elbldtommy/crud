6a99dbdce7285731da651dcbe86a67a135a5eb90
elbldtommy <tt37038@gmail.com>
2025-06-24 09:29:26 -0300

    Update crear.php

--- crear.php
+++ crear.php
@@ -1,1 +1,49 @@
+<?php
+include "conexion.php";
 
+if
+($_SERVER['REQUEST_METHOD']=='POST'){
+    $nombre=$_POST['nombre'];
+    $descripcion=$_POST['descripcion'];
+    $precio=$_POST['precio'];
+    $stock=$_POST['stock'];
+
+    $sql="INSERT INTO productos(nombre,descripcion,precio,stock)VALUES(?,?,?,?)";
+    $res= $conn->prepare($sql);
+    $res->execute($nombre, $descripcion, $precio, $stock);
+
+    header("Location:index.php? success=Producto creado");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet"
+    href="styles/styles.css">
+    <title>Producto</title>
+</head>
+<body>
+<h1>Agregar Nuevo</h1>    
+<form method="post">
+<label for="nombre">Nombre</label>
+<input type="text" name="nombre" required>
+<br>
+<label
+for="descripcion">Descripción</label>
+<textarea
+name="descripcion"></textarea>
+<br>
+<label for="precio">Precio</label>
+<input type="number" step="0.01" name="precio" required>
+<br>
+<label for="stock">Stock</label>
+<input type="number" name="stock" required>
+<br>
+<input for="submit" value="Guardar producto"><a href="index.php">Cancelar</a> 
+
+</form>
+</body>
+</html>
